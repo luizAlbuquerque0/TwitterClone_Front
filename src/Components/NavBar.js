c3457ccd82bf3b24b9c1100507a30754676a38e3
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-08-03 15:45:37 -0300

    Fix NavBar Links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,29 +4,39 @@ import { NavLink, Link } from "react-router-dom";
 import { SiTwitter } from "react-icons/si";
 import { BsPersonFill, BsSearch } from "react-icons/bs";
 
+import { useSelector } from "react-redux";
+
 const NavBar = () => {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav className={styles.nav}>
       <Link to="/" className={styles.brand}>
         <SiTwitter /> TwitterClone
       </Link>
-      <ul>
-        <li>
-          <NavLink to="/login">Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Cadatrar</NavLink>
-        </li>
-      </ul>
-      {/* <form>
-        <label>
-          <BsSearch className={styles.search} />
-          <input type="text" placeholder="Procure um tweet" />
-        </label>
-      </form>
-      <NavLink to="/profile" className={styles.profile}>
-        <BsPersonFill />
-      </NavLink> */}
+      {!user && (
+        <ul>
+          <li>
+            <NavLink to="/login">Entrar</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Cadatrar</NavLink>
+          </li>
+        </ul>
+      )}
+      {user && (
+        <>
+          <form>
+            <label>
+              <BsSearch className={styles.search} />
+              <input type="text" placeholder="Procure um tweet" />
+            </label>
+          </form>
+          <NavLink to="/profile" className={styles.profile}>
+            <BsPersonFill />
+          </NavLink>
+        </>
+      )}
     </nav>
   );
 };
