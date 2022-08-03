ef5c9220dcec17aa3cd8775920b9d8b5b4fae037
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-08-03 12:36:20 -0300

    Implement login,Registe and Logout Services

---
+++ src/Services/authService.js
@@ -0,0 +1,47 @@
+import { api, requestConfig } from "../Utils/config";
+
+//Register user
+const register = async (data) => {
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(`${api}/users/register`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+      const loginInfo = {
+        "Email" : res.Email,
+        "Password" : res.Password
+      }
+
+      login(loginInfo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const login = (data) => {
+ const config = requestConfig("PUT", data);
+
+ try {
+    const res = await fetch(`${api}/users/login`, config).then((res)=> res.json()).catch((err)=> err);
+
+    if(res){
+        localStorage.setItem("user", JSON.stringify(res));
+    }
+ } catch (error) {
+    console.log(error)
+ }
+}
+
+const logout = ()=>{
+    localStorage.removeItem("user");
+}
+
+const authService = {
+    register,
+    login,
+    logout
+}
+
+export default authService;
---
+++ src/Utils/config.js
@@ -0,0 +1,26 @@
+export const api = "https://localhost:7065";
+
+export const requestConfig = (method, data, token = null, image = null) => {
+  let config;
+
+  if (method === "DELETE" || data === null) {
+    config = {
+      method: method,
+      headers: {},
+    };
+  } else {
+    config = {
+      method: method,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
