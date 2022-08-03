b74939e26adb8dc183e221d035689f425ee583f5
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-08-03 15:14:12 -0300

    Implement login

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,14 +1,33 @@
 import styles from "./Auth.module.css";
-
-import { useState } from "react";
-
 import { Link } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { login, reset } from "../../Slices/authSlice";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handlerSubmit = () => {};
+  const dispatch = useDispatch();
+
+  const { loading } = useSelector((state) => state.auth);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      Email: email,
+      Password: password,
+    };
+
+    dispatch(login(user));
+  };
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return (
     <div className={styles.login}>
@@ -19,15 +38,18 @@ const Login = () => {
           placeholder="Email"
           value={email || ""}
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
         <input
           type="password"
           placeholder="Senha"
           value={password || ""}
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
 
-        <input type="submit" value="Cadastrar" />
+        {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
       </form>
       <p>
         Ainda não tem conta? <Link to="/register">Clique aqui</Link>
