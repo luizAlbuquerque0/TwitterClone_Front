a3176a22700dfe08178d680eef9fbddc4de65942
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-08-04 18:17:05 -0300

    implement tweet form

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,21 @@
 import styles from "./Home.module.css";
 
+import { useRef, useState } from "react";
+
 const Home = () => {
+  const [tweetContent, setTweetContent] = useState();
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className={styles.home}>
+      <h2>Página Inicial</h2>
+      <div className={styles.tweetar}>
+        <textarea
+          placeholder="O que está acontecendo?"
+          onChange={(e) => setTweetContent(e.target.value)}
+          maxLength="150"
+        />
+        <button>tweetar</button>
+      </div>
     </div>
   );
 };
